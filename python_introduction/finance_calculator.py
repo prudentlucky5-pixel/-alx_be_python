2fcd314fd02623af880522b361af02a422e5bab2
prudent <prudentlucky5@gmail.com>
2025-11-29 23:06:23 +0100

    added finance-calculator.py

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+monthly_income = 5000
+monthly_expenses = 4000
+savings = monthly_income - monthly_expenses
+print("Your monthly savings are:", savings)
+projected_annual_savings = savings * 12 + (savings * 12 * 0.05)
+print("Projected annual savings:", projected_annual_savings)
