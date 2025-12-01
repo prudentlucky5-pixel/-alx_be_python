4e9cec3796dac643b64423e2c132ee569f38e421
prudentlucky5-pixel <prudentlucky5@gmail.com>
2025-12-01 21:35:44 +0100

    Update basic_operation.py

--- python_introduction/basic_operation.py
+++ python_introduction/basic_operation.py
@@ -1,8 +1,10 @@
 number_1 = 10
 number_2 = 5
+
 sum_result = number_1 + number_2
 difference_result = number_1 - number_2
 product_result = number_1 * number_2
+
 print("Sum:", sum_result)
 print("Difference:", difference_result)
 print("Product:", product_result)
